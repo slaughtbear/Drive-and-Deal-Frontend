d5ee607c8094da775174a9971063f9f5a8208383
slaughtbear <ivanaguirre368@gmail.com>
2025-08-07 00:08:08 -0600

    fix: Renderizado de login

--- src/App.jsx
+++ src/App.jsx
@@ -2,48 +2,49 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./pages/Login";
 import Cars from './pages/Cars';
 import Customers from './pages/Customers';
-import Rentals from './pages/Rentals'
-import Repairs from './pages/Repairs'
+import Rentals from './pages/Rentals';
+import Repairs from './pages/Repairs';
 
 import CarsForm from "./pages/CarForm";
 import CustomersForm from "./pages/CustomersForm";
 import RentalsForm from "./pages/RentalsForm";
-import RepairsForm from "./pages/RepairsForm"
+import RepairsForm from "./pages/RepairsForm";
 
 import Sidebar from "./components/sidebar/Sidebar";
 
 function App() {
   return (
     <BrowserRouter>
-        <div className="flex container m-auto px-10">
-          <Sidebar />
-          <Routes>
-            <Route path="" element={<Login />}></Route>
+      <Routes>
+        {/* Ruta de login sin Sidebar */}
+        <Route path="/" element={<Login />} />
+        
+        {/* Rutas protegidas con Sidebar */}
+        <Route path="/dashboard/*" element={
+          <div className="flex container m-auto px-10">
+            <Sidebar />
+            <Routes>
+              <Route path="cars" element={<Cars />} />
+              <Route path="cars/:id" element={<CarsForm />} />
+              <Route path="cars/add" element={<CarsForm />} />
 
+              <Route path="customers" element={<Customers />} />
+              <Route path="customers/:id" element={<CustomersForm />} />
+              <Route path="customers/add" element={<CustomersForm />} />
 
-            <Route path="/dashboard/cars" element={<Cars />}></Route>
-            <Route path="/dashboard/cars/:id" element={<CarsForm />}></Route>
-            <Route path="/dashboard/cars/add" element={<CarsForm />}></Route>
+              <Route path="rentals" element={<Rentals />} />
+              <Route path="rentals/:id" element={<RentalsForm />} />
+              <Route path="rentals/add" element={<RentalsForm />} />
 
-
-            <Route path="/dashboard/customers" element={<Customers />}></Route>
-            <Route path="/dashboard/customers/:id" element={<CustomersForm />}></Route>
-            <Route path="/dashboard/customers/add" element={<CustomersForm />}></Route>
-
-
-            <Route path="/dashboard/rentals" element={<Rentals />} />
-            <Route path="/dashboard/rentals/:id" element={<RentalsForm />} />
-            <Route path="/dashboard/rentals/add" element={<RentalsForm />}></Route>
-
-
-            <Route path="/dashboard/repairs" element={<Repairs />} />
-            <Route path="/dashboard/repairs/:id" element={<RepairsForm />} />
-            <Route path="/dashboard/repairs/add" element={<RepairsForm />}></Route>
-          </Routes>
-        </div>
+              <Route path="repairs" element={<Repairs />} />
+              <Route path="repairs/:id" element={<RepairsForm />} />
+              <Route path="repairs/add" element={<RepairsForm />} />
+            </Routes>
+          </div>
+        } />
+      </Routes>
     </BrowserRouter>
   );
 }
 
 export default App;
-
